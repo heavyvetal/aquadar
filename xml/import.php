a4ee616683a204f0ef6576b19f0b3776163129e5
Виталий <x_15@mail.ru>
2021-04-01 15:19:21 +0300

    addded xml importer

---
+++ xml/import.php
@@ -0,0 +1,15 @@
+<?php
+$xmlString = file_get_contents(__DIR__. "\products.xml");
+
+$xml = new SimpleXMLElement($xmlString);
+
+foreach ($xml->item as $item) {
+    echo $item->title;
+    echo "<br>";
+    echo $item->price;
+    echo "<br>";
+    echo $item->code;
+    echo "<br>";
+    echo $item->brand;
+    echo "<br>";
+}
