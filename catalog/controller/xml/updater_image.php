ce341edc9d140c7c98fd67fef2eb228d7a22ee67
Виталий <x_15@mail.ru>
2021-07-08 21:45:06 +0300

    08.07 image updater3

--- catalog/controller/xml/updater_image.php
+++ catalog/controller/xml/updater_image.php
@@ -28,52 +28,58 @@ class ControllerXmlUpdaterImage extends Controller {
 
                 // Ищем в базе товары с заполненным sku
                 if ($code != '') {
-                    $product = $this->getProduct($title);
-                    //print_r($product);
+                    $products = $this->getProduct($title);
+                    //print_r($products);
 
                     // такой артикул в бд найден
-                    if ($product) {
-                        $product_id = $product['product_id'];
-                        $path_dir = $path.$code;
+                    if ($products) {
+                        // в базе есть дубли с одинаковыми названиями товаров
+                        // пишем ссылки в дубль тоже
+                        foreach ($products as $product) {
+                            $product_id = $product['product_id'];
+                            $path_dir = $path.$code;
 
-                        echo "$counter :: $code :: $title >> ".$product_id."<br>\n";
+                            echo "$counter :: $code :: $title >> ".$product_id."<br>\n";
 
-                        // создаем папку картинок товара
-                        if (!file_exists($path_dir)) {
-                            mkdir($path_dir, 0777);
-                        }
-
-                        if (!file_exists($path_dir)) {
-                            echo "<span style='color:red;'> Папка не была создана: $path_dir </span>";
-                        }
-
-                        $sort_order = 10;
-
-                        foreach ($images as $image) {
-                            $image_link = $image;
-                            $image_link_parts = explode('/', $image_link);
-                            $image_link_end = end($image_link_parts);
-                            $full_name_dir = $path_dir.'/'.$image_link_end;
-                            $link_in_db = 'catalog/products_additional/'.$code.'/'.$image_link_end;
-                            //echo ">>>$link_in_db sort_order=$sort_order ";
-
-                            if (!$this->isInDb($link_in_db, $product_id)) {
-                                echo "Будет записана<br>\n";
-                                $this->insertIntoDb($product_id, $link_in_db, $sort_order);
-                            } else {
-                                echo "Уже в базе<br>\n";
+                            // создаем папку картинок товара
+                            if (!file_exists($path_dir)) {
+                                mkdir($path_dir, 0777);
                             }
 
-                            // Проеверяем, есть ли уже такое изображение в папке
-                            if ($image_link_end != '' && file_exists($full_name_dir)) {
-                                echo "такая уже есть - ".$full_name_dir."<br>\n";
-                            } else {
-                                echo "такой нет - ".$full_name_dir."<br>\n";
-                                file_put_contents($full_name_dir, file_get_contents($image_link));
+                            if (!file_exists($path_dir)) {
+                                echo "<span style='color:red;'> Папка не была создана: $path_dir </span>";
                             }
 
-                            $sort_order += 10;
+                            $sort_order = 10;
+
+                            foreach ($images as $image) {
+                                $image_link = $image;
+                                $image_link_parts = explode('/', $image_link);
+                                $image_link_end = end($image_link_parts);
+                                $full_name_dir = $path_dir.'/'.$image_link_end;
+                                $link_in_db = 'catalog/products_additional/'.$code.'/'.$image_link_end;
+                                //echo ">>>$link_in_db sort_order=$sort_order ";
+
+                                if (!$this->isInDb($link_in_db, $product_id)) {
+                                    echo "Будет записана<br>\n";
+                                    $this->insertIntoDb($product_id, $link_in_db, $sort_order);
+                                } else {
+                                    echo "Уже в базе<br>\n";
+                                }
+
+                                // Проеверяем, есть ли уже такое изображение в папке
+                                if ($image_link_end != '' && file_exists($full_name_dir)) {
+                                    echo "такая уже есть - ".$full_name_dir."<br>\n";
+                                } else {
+                                    echo "такой нет - ".$full_name_dir."<br>\n";
+                                    file_put_contents($full_name_dir, file_get_contents($image_link));
+                                }
+
+                                $sort_order += 10;
+                            }
+
                         }
+
                     }
                 }
                 $counter++;
@@ -83,16 +89,17 @@ class ControllerXmlUpdaterImage extends Controller {
         }
     }
 
+    /**
+     * @param string $title
+     * @return array
+     */
     private function getProduct($title)
     {
         $product = $this->db->query("
-            SELECT pd.product_id
+            SELECT DISTINCT pd.product_id
             FROM `oc_product_description` pd 
             WHERE pd.name='" . $this->db->escape($title) . "'
-            LIMIT 1
         ")->rows;
-        
-        if ($product) $product = $product[0];
         
         return $product;
     }
